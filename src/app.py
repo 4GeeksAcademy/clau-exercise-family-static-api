909cb074e53f4bc05661587b11d0985f4ed04172
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-09-06 15:59:07 +0000

    family pronto

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -25,18 +26,62 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# // crear mis empoints
+
+
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
-    # this is how you can use the Family datastructure by calling its methods
+    # estructura de datos de familia llamado a sus metodos
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+#2 obtener un solo  de la familia
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_single_member(member_id):
 
 
-    return jsonify(response_body), 200
+    # Hago una consulta a la tabla FamilyStructure para que traiga un miembro de la familia
+    member = jackson_family.get_member(member_id)
+
+    # Responde mostrando el miembro consultado
+
+    return jsonify(member), 200
+
+# 3 Añadir un miembro a la familia
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member=json.loads(request.data)
+    jackson_family.add_member(new_member)
+    return jsonify(new_member), 200
+
+# 3 Borrar un miembro a la familia
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+
+    jackson_family.delete_member(member_id)
+
+    return jsonify({"done": True}), 200
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
